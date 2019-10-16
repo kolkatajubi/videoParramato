880ec8661ace1a3ba4043051a0424fcaf58909ff
kolkatajubi <kolkatachamps@jubi.ai>
2019-10-16 14:23:44 +0530

    validation-regex

--- assets/files/moduleRunner.js
+++ assets/files/moduleRunner.js
@@ -507,7 +507,7 @@ function createButtonWebView(data, text) {
 function createText(pattern) {
   if (pattern == undefined) {
     console.log("Create Text Input");
-    return `<input id='name' class='response-text' type='text' onkeyup='validate("/[\w]+/");' placeholder='enter here ...' /> <button class='send' style='display:none;' onclick='getNextStageData();'>Send</button>`;
+    return `<input id='name' class='response-text' type='text' onkeyup='validate("/[\\w]+/");' placeholder='enter here ...' /> <button class='send' style='display:none;' onclick='getNextStageData();'>Send</button>`;
   } else {
     return (
       `<input id='name' class='response-text' type='text' onkeyup='validate(` +
