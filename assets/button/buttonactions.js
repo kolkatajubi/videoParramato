8215c9d26a3e57537ea5d70199a378fbec95b8f8
Souvik Das <souvik131@gmail.com>
2019-10-21 18:58:00 +0530

    fullscreen on touch added

---
+++ assets/button/buttonactions.js
@@ -0,0 +1,23 @@
+var fulscreen = 0;
+function FS() {
+  // console.log("fullscreen called...", fullscreen);
+  if (fullscreen == 0) {
+    if (document.body.requestFullscreen) document.body.requestFullscreen();
+    else if (document.body.mozRequestFullScreen)
+      document.body.mozrequestFullscreen();
+    else if (document.body.webkitRequestFullscreen)
+      document.body.webkitRequestFullscreen();
+    else if (document.body.msRequestFullscreen)
+      document.body.msRequestFullscreen();
+    fullscreen = 1;
+    // document.getElementById("fs").innerHTML = "EXIT FULLSCREEN";
+    $(".display")
+      .width("100%")
+      .height("100%");
+  }
+}
+
+// function toggleFS() {
+//   // console.log("toggle fullscreen called...", fullscreen);
+//   if (fullscreen == 0) FS();
+// }
