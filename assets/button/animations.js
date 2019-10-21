46e8f3dabcf548400b0df53601259690752dd609
kolkatajubi <kolkatachamps@jubi.ai>
2019-10-21 23:27:09 +0530

    msg

--- assets/button/animations.js
+++ assets/button/animations.js
@@ -1,10 +1,6 @@
-function clickThis(el) {
-  let width = $(el).width();
-  setInterval(() => {
-    if (width > 20) {
-      width -= 0.5;
-      $(el).html("i");
-      $(el).css("width", width + "px");
-    }
-  }, 5);
+function run(container) {
+  container.addEventListener("animationend", () => {
+    container.classList.remove("active");
+    container.classList.add("remove");
+  });
 }
