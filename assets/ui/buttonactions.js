292c350926b90b58cfd29485bf2dab75c34612c6
kolkatajubi <kolkatachamps@jubi.ai>
2019-10-23 14:41:04 +0530

    button update

--- assets/ui/buttonactions.js
+++ assets/ui/buttonactions.js
@@ -14,6 +14,7 @@ function run(button) {
   button.addEventListener("animationend", () => {
     button.classList.remove("active");
     button.classList.add("remove");
+    getNextStageData();
   });
   getSiblings(button).forEach(el => {
     el.style.opacity = "0.2";
