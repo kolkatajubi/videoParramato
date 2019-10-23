f4d975e4e21ea56b4bd4544a323bd6465ffb03e8
kolkatajubi <kolkatachamps@jubi.ai>
2019-10-23 15:09:48 +0530

    button update

--- assets/files/includeAll.js
+++ assets/files/includeAll.js
@@ -1,10 +1,6 @@
 document.write(`<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://pixie.jubi.ai/videoParramato/static/files/moduleRunner.js"></script>
-    <link
-      rel="stylesheet"
-      id="stylesheet"
-      href="https://pixie.jubi.ai/videoParramato/static/css/stylexls.css"
-    />
+    
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
