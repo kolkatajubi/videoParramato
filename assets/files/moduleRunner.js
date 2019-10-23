7e6fb06a57b7bca005f1ed531219c24660ceb519
kolkatajubi <kolkatachamps@jubi.ai>
2019-10-23 18:10:05 +0530

    fullscreen fix

--- assets/files/moduleRunner.js
+++ assets/files/moduleRunner.js
@@ -333,23 +333,23 @@ function playPause() {
   }
 }
 
-// function FS() {
-//   // console.log("fullscreen called...", fullscreen);
-//   if (fullscreen == 0) {
-//     if (document.body.requestFullscreen) document.body.requestFullscreen();
-//     else if (document.body.mozRequestFullScreen)
-//       document.body.mozrequestFullscreen();
-//     else if (document.body.webkitRequestFullscreen)
-//       document.body.webkitRequestFullscreen();
-//     else if (document.body.msRequestFullscreen)
-//       document.body.msRequestFullscreen();
-//     fullscreen = 1;
-//     // document.getElementById("fs").innerHTML = "EXIT FULLSCREEN";
-//     $(".display")
-//       .width("100%")
-//       .height("100%");
-//   }
-// }
+function FS() {
+  // console.log("fullscreen called...", fullscreen);
+  if (fullscreen == 0) {
+    if (document.body.requestFullscreen) document.body.requestFullscreen();
+    else if (document.body.mozRequestFullScreen)
+      document.body.mozrequestFullscreen();
+    else if (document.body.webkitRequestFullscreen)
+      document.body.webkitRequestFullscreen();
+    else if (document.body.msRequestFullscreen)
+      document.body.msRequestFullscreen();
+    // fullscreen = 1;
+    // document.getElementById("fs").innerHTML = "EXIT FULLSCREEN";
+    $(".display")
+      .width("100%")
+      .height("100%");
+  }
+}
 
 // function exitFS() {
 //   // console.log("exit fullscreen called...", fullscreen);
