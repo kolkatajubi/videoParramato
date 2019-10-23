510dfc4c0a1cd550fd36e8af52916ce815d07e0d
kolkatajubi <kolkatachamps@jubi.ai>
2019-10-23 13:31:23 +0530

    button update

--- assets/files/includeAll.js
+++ assets/files/includeAll.js
@@ -26,4 +26,9 @@ document.write(`<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/
       href="https://pixie.jubi.ai/videoParramato/static/files/car.css"
     />
     <script src="https://pixie.jubi.ai/videoParramato/static/ui/buttonactions.js"></script>
+    <link rel="stylesheet" href="https://pixie.jubi.ai/videoParramato/static/ui/button.css" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Muli:700&display=swap"
+      rel="stylesheet"
+    />
     `);
