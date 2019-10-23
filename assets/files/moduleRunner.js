1fa801d2d82bdf143779bce27ea0fe07c8cab3fb
kolkatajubi <kolkatachamps@jubi.ai>
2019-10-23 15:23:44 +0530

    button update

--- assets/files/moduleRunner.js
+++ assets/files/moduleRunner.js
@@ -388,7 +388,7 @@ function videoDisplay(videoData) {
   // console.log("<source id='start' type='video/mp4' src='" + videoData + "' />");
   // $("#myVideo").attr("poster", "");
   var video = document.getElementById("myVideo");
-  // console.log(video);
+  console.log(video);
   video.load();
   if (currentStageNum == 0) $("#playImg").show();
   else video.play();
